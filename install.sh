f7a361aedd5ef49c042eadab9bea42e5d3e4dc74
KP Kaiser <kirk@zothcorp.com>
2023-02-16 18:35:06 +0000

    adding an eval here, and there

--- install.sh
+++ install.sh
@@ -1,2 +1,1 @@
-gpgconf --kill gpg-agent
-git config --global gpg.format ssh && git config --global user.signingkey "$SSH_SIGNING_KEY" && git config commit.gpgsign true --global
+eval $(ssh-agent -s) && git config --global gpg.format ssh && git config --global user.signingkey "$SSH_SIGNING_KEY" && git config commit.gpgsign true --global
