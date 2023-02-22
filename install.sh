ebb025f0ce8dac4f464f7437d0c1e315d50b1604
KP Kaiser <kirk@zothcorp.com>
2023-02-22 15:24:32 -0500

    Get rid of erroneus eval

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-eval $(ssh-agent -s) && git config --global gpg.format ssh && git config --global user.signingkey "$SSH_SIGNING_KEY" && git config commit.gpgsign true --global
+git config --global gpg.format ssh && git config --global user.signingkey "$SSH_SIGNING_KEY" && git config commit.gpgsign true --global
